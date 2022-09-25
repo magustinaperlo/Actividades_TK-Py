a455755a232add836209453aedf3ea73bb4d6042
Agustina Aperlo <43002670+magustinaperlo@users.noreply.github.com>
2022-09-25 18:46:07 -0300

    Update Peliculas.py
    Corrección en el código

--- Peliculas.py
+++ Peliculas.py
@@ -1,19 +1,27 @@
 from tkinter import *
+from tkinter import messagebox
 
 
 
 def Añadir():
     peli = peliculas.get() 
-    lista.insert(END, peli) 
-    peliculas.delete(0, END) 
+    # lista.insert(END, peli) 
+    # peliculas.delete(0, END) 
+    if (peli.isspace() or len(peli) <= 1):
+        messagebox.showinfo(message="El nombre de la película no debe comenzar con un espacio", title="Error")
+        peliculas.delete(0,END)
+    else:
+        lista.insert(END,peli)
+        peliculas.delete(0,END)
 
 
 ventana = Tk()
 ventana.geometry("300x250")
 ventana.title("Peliculas")
-
-peliculas = StringVar()
-peliculas.set("")
+#cuidado las variables no deben llamarse igual
+# peliculas = StringVar()
+peliculasVar = StringVar()
+peliculasVar.set("")
 
 framePeli = Frame(ventana)
 framePeli.pack()
@@ -24,7 +32,8 @@ lbl_pelicula.grid(row=0, column=0)
 lbl_list_pelicula = Label(framePeli, text="Peliculas")
 lbl_list_pelicula.grid(row=0, column=1)
 
-peliculas = Entry(framePeli, textvariable=peliculas)
+#cuidado las variables no deben llamarse igual
+peliculas = Entry(framePeli, textvariable=peliculasVar)
 peliculas.grid(row=1, column=0)
 
 lista = Listbox(framePeli)
